353f2feed5e61eeabdc8414cc0537b044b4dbd6a
Yusuke Yamamoto <yusuke@mac.com>
2023-10-08 09:42:11 +0900

    無駄な修正

---
+++ public/fizzbuzz.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This method generates the FizzBuzz sequence from 1 to 100 and outputs it to the console.
+ * Numbers divisible by 3 are replaced with "Fizz", numbers divisible by 5 are replaced with "Buzz",
+ * and numbers divisible by both 3 and 5 are replaced with "FizzBuzz".
+ *
+ * @return void
+ */
+
+function fizzBuzz(): void
+{
+    $result = [];
+    for ($i = 1; $i <= 100; $i++) {
+        $fizz = $i % 3 == 0;
+        $buzz = $i % 5 == 0;
+
+        if ($fizz && $buzz) {
+            $result[$i] = "FizzBuzz";
+        } elseif ($fizz) {
+            $result[$i] = "Fizz";
+        } elseif ($buzz) {
+            $result[$i] = "Buzz";
+        } else {
+            $result[$i] = (string)$i;
+        }
+    }
+
+    foreach ($result as $value) {
+        echo $value . "\n";
+    }}
+
+fizzBuzz();
+
